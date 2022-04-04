3d797173b99528193552e7cabc9d7e10c2bb7d36
gdumani <gdumani@gmail.com>
2022-04-04 15:40:06 -0600

    created store, files for reduces and update index

---
+++ src/redux/configureStore.jsx
@@ -0,0 +1,12 @@
+import { applyMiddleware, combineReducers, createStore } from 'redux';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+
+const reducer = combineReducers({});
+
+const store = createStore(
+  reducer,
+  applyMiddleware(thunk, logger),
+);
+
+export default store;
